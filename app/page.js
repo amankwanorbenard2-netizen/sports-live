82fbaaf210fc0f5cc71ac2567de6419483ecc10f
Benard <amankwanorbenard2@gmail.com>
2026-05-12 20:02:05 +0000

    final fix

--- app/page.js
+++ app/page.js
@@ -1,15 +1,20 @@
 export default function Page() {
   return (
-    <div style={{
-      background: "black",
-      color: "white",
-      minHeight: "100vh",
-      padding: "20px"
-    }}>
-      <h1 style={{
-        color: "#39ff14",
-        fontSize: "50px"
-      }}>
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        minHeight: "100vh",
+        padding: "20px",
+      }}
+    >
+      <h1
+        style={{
+          color: "#39ff14",
+          fontSize: "50px",
+          fontWeight: "bold",
+        }}
+      >
         Sports Live
       </h1>
 
